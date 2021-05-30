97b07d1d48fb5d3d64ec67cf0088383888f0725c
Mdilow <25sekhwelad@gmail.com>
2021-05-30 11:26:55 +0200

    Added Extensions for Database Migration

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,3 +1,4 @@
+using Discount.API.Extensions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -7,13 +8,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Discount.API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            host.MigrateDatabase<Program>();
+            host.Run();
+
+            //CreateHostBuilder(args)
+            //    .Build()
+            //    .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
